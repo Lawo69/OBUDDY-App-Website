8fc443752eb0c1ef8d6df9ce82135b72385684bf
Supun Gunawardana <supungunawardana.me@gmail.com>
2023-12-04 20:46:16 +0530

    finished commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,52 @@
 $(function () {
-    $(document).ready(function(){
+    "use strict"
+    $(window).on('load', function (event) {
+        $('.preloader').delay(500).fadeOut(500);
+    });
+    $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".navbar-area").removeClass("sticky");
+        } else {
+            $(".navbar-area").addClass("sticky")
+        }
+    });
+
+    $(document).ready(function () {
         $('.venobox').venobox();
     });
 
+    // wow js
     new WOW().init();
-})
+
+    // tiny slider
+    var slider = tns({
+        container: '.testimonial-active',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        mouseDrag: true,
+        nav: false,
+        controlsText: ['<i class="fas fa-angle-left prev"></i>', '<i class="fas fa-angle-right next"></i>'],
+    });
+
+    //   scrollit js
+    $.scrollIt();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get all navigation links
+    const navLinks = document.querySelectorAll('.navbar-nav a');
+  
+    // Add click event listener to each link
+    navLinks.forEach(link => {
+      link.addEventListener('click', function () {
+        // Remove 'active' class from all links
+        navLinks.forEach(link => link.classList.remove('active'));
+  
+        // Add 'active' class to the clicked link
+        this.classList.add('active');
+      });
+    });
+  });
+  
